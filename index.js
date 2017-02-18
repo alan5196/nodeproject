175ff3a3db3976054a2b982ad6b5bac8835e9597
Alan Lin <alan5196@icloud.com>
2017-02-18 21:58:37 +0800

    Adding index.js to manipulate stream object.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello NodeJS");
+var CountStream = require("./countstream");
+var countStream = new CountStream('book');
+var http = require('http');
+
+http.get('http://bookzz.org', function(res){
+	res.pipe(countStream);
+});
+
+countStream.on('total', function(count){
+	console.log("Total count:" + count);
+});
